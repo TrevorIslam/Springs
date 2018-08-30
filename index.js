ab43c06d87db153fe7bbb2ead9af9db4d2839858
mvcodeclub <sarah@mvcodeclub.com>
2018-08-29 18:12:50 -0700

    changes

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 var springs;
 var t = 0;
+var num = 20;
+var s = 10;
+
 
 function setup () {
   createCanvas(500,500,WEBGL);
+    normalMaterial();
   springs = [];
-  for (let i =0;i<10;i++) {
-    springs.push(new Spring(i*20,0,0,20));
+  for (let i =0;i<num;i++) {
+    for (let j = 0;j<num;j++) {
+      let x = i*s-num*s/2+s/2;
+      let y = 0;
+      let z = j*s-num*s/2+s/2;
+      let d = sqrt(x*x+y*y+z*z);
+      let w = num*s;
+      let maxd = sqrt((w*w)/2);
+      let scaler = pow(d/maxd,3);
+      let phase = PI*scaler;
+      springs.push(new Spring(x,y,z,s,phase));
+    }
   }
 };
 
 function draw () {
   background(50);
   push();
-    translate(-50,100,50);
+    translate(0,0,50);
     rotateX(-PI/6);
     rotateY(PI/6);
     for (let i of springs) {
--- spring.js
+++ spring.js
@@ -1,15 +1,16 @@
 class Spring {
-  constructor (x,y,z,size) {
+  constructor (x,y,z,size,phase) {
     this.x = x;
     this.y = y;
     this.z = z
     this.size = size;
     this.h = 100;
+    this.phase = phase;
   }
 
   draw () {
     push();
-      this.h = 50 + (200* abs(sin(t/50)));
+      this.h = 50 + (100* abs(sin(t/40+this.phase)));
       translate(this.x,this.y,this.z);
       box(this.size,this.h,this.size);
     pop();
